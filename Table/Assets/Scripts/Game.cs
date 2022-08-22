9e4060b722a20adf509096ede3dbf1e8d94363c6
FedrovSergey <serzh-fedorov-2004@mail.ru>
2022-08-22 22:20:53 +0300

    first

---
+++ Table/Assets/Scripts/Game.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] int Score;
+
+    public GameObject InternetPanel;
+    public GameObject ClikerPanel;
+    public GameObject HackPanel;
+    public GameObject ThingPanel1;
+    public GameObject ThingPanel2;
+    public GameObject ResultPanel;
+
+    public Text ScoreText;
+    public void OnClickButton()
+    {
+        Score++;
+    }
+
+    private void Update()
+    {
+        ScoreText.text = Score + "₽";
+    }
+
+    public void ShowAndHideInternetPanel()
+    {
+        InternetPanel.SetActive(!InternetPanel.activeSelf);
+    }
+    public void ShowAndHideClikerPanel()
+    {
+        ClikerPanel.SetActive(!ClikerPanel.activeSelf);
+    }
+    public void ShowAndHideHackPanel()
+    {
+        HackPanel.SetActive(!HackPanel.activeSelf);
+    }
+    public void ShowAndHideThingPanel1()
+    {
+        ThingPanel1.SetActive(!ThingPanel1.activeSelf);
+    }
+    public void ShowAndHideThingPanel2()
+    {
+        ThingPanel2.SetActive(!ThingPanel2.activeSelf);
+    }
+    public void ShowAndHideResultPanel()
+    {
+        ResultPanel.SetActive(!ResultPanel.activeSelf);
+    }
+
+    //public void EnterText()
+    //{
+
+    //}
+}
